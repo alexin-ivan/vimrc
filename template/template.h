99cc5ad7f7af09c502829af1570f56197b595f11
alexin-ivan <alexin.ivan@gmail.com>
2013-04-17 20:46:43 +0400

    inital commit

---
+++ template/template.h
@@ -0,0 +1,8 @@
+// created: <+DATE+>
+#ifndef <+FILE_NAME+>_H_INCLUDED
+#define <+FILE_NAME+>_H_INCLUDED
+
+<+CURSOR+>
+
+#endif // <+FILE_NAME+>_H_INCLUDED
+
---
+++ template/template.hs
@@ -0,0 +1,18 @@
+--
+-- Author: Ivan
+-- File: <+FileName+>.hs
+-- Data: <+DATE+>
+
+
+module <+FileName+> where
+
+
+
+test :: IO ()
+test = do
+    return ()
+
+main :: IO ()
+main = do
+    print "Hello, World!"
+    return ()
